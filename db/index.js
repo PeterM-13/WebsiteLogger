523f5cf9967a03c5b16d60666306a05e7e72ca3e
Peter <petershotbox@gmail.com>
2025-03-10 22:19:37 +0000

    Progress

--- db/index.js
+++ db/index.js
@@ -1,7 +1,5 @@
 import pg from "pg";
 
 export const pool = new pg.Pool({
-  connectionString: 'postgresql://postgres:xxlNJFjKWY8IU6kJ@socially-heuristic-fish.data-1.use1.tembo.io:5432/postgres',//process.env.DATABASE_URL,
-  min: 0,
-  max: 10,
+  connectionString: process.env.DATABASE_URL,
 });
