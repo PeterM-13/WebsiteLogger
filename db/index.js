078e982207427bec83fc9ea456b37f01fe0fac26
Peter <petershotbox@gmail.com>
2025-03-10 22:28:32 +0000

    Added error handling

--- db/index.js
+++ db/index.js
@@ -3,3 +3,7 @@ import pg from "pg";
 export const pool = new pg.Pool({
   connectionString: process.env.DATABASE_URL,
 });
+
+pool.on("error", (err, client) => {
+  console.log(`Error received on db pool: ${err.message} \n ${err.stack}`);
+});
