1993ded3708cfdc8c67479ebf48fbc6fe9240a51
Peter <petershotbox@gmail.com>
2025-03-11 22:30:34 +0000

    Scrapped db, added emails

--- db/index.js
+++
@@ -1,9 +0,0 @@
-import pg from "pg";
-
-export const pool = new pg.Pool({
-  connectionString: process.env.DATABASE_URL,
-});
-
-pool.on("error", (err, client) => {
-  console.log(`Error received on db pool: ${err.message} \n ${err.stack}`);
-});
--- routes/index.js
+++ routes/index.js
@@ -1,36 +1,29 @@
 import express from "express";
 const router = express.Router();
-
-import {
-  addVisit,
-  addEmail,
-} from "../models/index.js";
-
-
+import nodemailer from 'nodemailer';
 
 // POST to add a visit
-router.post("/visit/:url", async (req, res) => {
-  try {
-    const result = await addVisit(req.params.url);
-    res.status(201).json({
-      success: true,
-      payload: result,
-    });
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: error.message,
-    });
-  }
-});
+// router.post("/visit/:url", async (req, res) => {
+//   try {
+//     //const result = await addVisit(req.params.url);
+//     res.status(201).json({
+//       success: true,
+//       payload: result,
+//     });
+//   } catch (error) {
+//     res.status(500).json({
+//       success: false,
+//       message: error.message,
+//     });
+//   }
+// });
 
 // POST to add an email
 router.post("/email/:email", async (req, res) => {
   try {
-    const result = await addEmail(req.params.email);
+    const success = send_email("New Pre-Order", `'${req.params.email}' has pre-ordered a new smart stick!`);
     res.status(201).json({
-      success: true,
-      payload: result,
+      success: success,
     });
   } catch (error) {
     res.status(500).json({
@@ -40,6 +33,41 @@ router.post("/email/:email", async (req, res) => {
   }
 });
 
+
+
+
+async function send_email(subject, message) {
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.mailersend.net',
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: process.env.email,
+      pass: process.env.password
+    },
+    tls: {
+      rejectUnauthorized: false,
+      secureProtocol: 'TLSv1_2_method' // You can try TLSv1_2_method or TLSv1_3_method
+  }
+  });
+
+  const mailOptions = {
+    from: process.env.emailFrom,
+    to: process.env.emailDest,
+    subject: subject,
+    text: message
+  };
+
+  //console.log("About to send email to: ", process.env.emailDest, " From: ", process.env.email)
+  try {
+    const info = await transporter.sendMail(mailOptions);
+    console.log(`Message sent: ${info.messageId}`);
+    return true
+  } catch (error) {
+    console.error("Error occurred while sending email:", error);
+    return false
+  }
+}
 
 
 
